0bf8f5b3859621b4fc71b04e9d4a8ebc1503753e
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-18 17:01:17 +0530

    SimplyLinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,9 +1,23 @@
+package com.bridglabz.programs;
 
-package com.bl.program.day14;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
+public class LinkedList {
+	private static Node head;
+	public static class Node {
+		private int data;
+		private Node next;
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+		
+		public static void main(String[] args) {
+			LinkedList linkedList = new LinkedList();
+			linkedList.head = new Node(56);
+			Node secondNode = new Node(30);
+			Node thirdNode = new Node(70);
+			linkedList.head.next = secondNode;
+			secondNode.next = thirdNode;
+		}
+	}
 }
